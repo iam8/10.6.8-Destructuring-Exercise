c5fab14d841a6e66587d50d2a0d8a1185a5776db
alexm <alex-m@outlook.com>
2022-10-14 16:08:41 -0700

    Copied ES5 code and added logic for the problems on Object and Array Destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,3 +1,90 @@
 // Ioana Alex Mititean
 // 10/14/22
 // Exercise 10.6.8: Destructuring
+
+
+// Object Destructuring 1
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+
+// Object Destructuring 2
+let planetFacts = {
+    numPlanets2: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+};
+
+let {numPlanets2, ...discoveryYears} = planetFacts;
+console.log(discoveryYears); // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+
+// Object Destructuring 3
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+
+getUserData({firstName: "Alejandro", favoriteColor: "purple"});
+// Your name is Alejandro and you like purple
+
+getUserData({firstName: "Melissa"});
+// Your name is Melissa and you like green
+
+getUserData({});
+// Your name is undefined and you like green
+
+
+// Array Destructuring 1
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
+
+
+// Array Destructuring 2
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+]
+
+console.log(raindrops); // Raindrops on roses
+console.log(whiskers); // Whiskers on kittens
+console.log(aFewOfMyFavoriteThings);
+// ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+
+// Array Destructuring 3
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]];
+
+console.log(numbers) // [10, 30, 20]
+
+
+// ES2015 Refactoring ------------------------------------------------------------------------------
+
+// ES5 Assigning Variables to Object Properties
+var obj0 = {
+    numbers: {
+        a: 1,
+        b: 2
+    }
+};
+
+var a0 = obj0.numbers.a;
+var b0 = obj0.numbers.b;
+
+// ES2015 Assigning Variables to Object Properties
+const obj1 = {
+    numbers: {
+        a: 1,
+        b: 2
+    }
+}
+
